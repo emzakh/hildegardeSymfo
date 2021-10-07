c93d92f637634b01ac954dac658831cb4e0e7152
emzakh <massimino.gutierrezmantione@gmail.com>
2021-10-07 14:32:00 +0200

    modif updateUserController

--- src/Controller/UpdateUserController.php
+++ src/Controller/UpdateUserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\UserImgModify;
 use App\Entity\PasswordUpdate;
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Component\Form\FormError;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,141 +18,28 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UpdateUserController extends AbstractController
 { 
-   
     /**
      * @IsGranted("ROLE_USER")
      * @param Request $request
-     * @param EntityManagerInterface $manager
+     * @param EntityManagerInterface $manager     
      * @return Response
      */
-    public function profile(Request $request, EntityManagerInterface $manager)
+    public function __invoke(Request $request, EntityManagerInterface $manager, User $user)
     {
-        $user = $this->getUser(); // récupérer l'utilisateur connecté
-        $form = $this->createForm(AccountType::class,$user);
-        $currentFile = $user->getPicture();
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $file = $form['picture']->getData();
-            if(!empty($file)){
-                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                $safeFilename = $originalFilename.'-'.rand();
-                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
-                try{
-                    $file->move(
-                        $this->getParameter('uploads_directory'),
-                        $newFilename
-                    );
-                }
-                catch(FileException $e)
-                {
-                    return $e->getMessage();
-                }
-                $user->setPicture($newFilename);
-            }else{
-                if(!empty($currentFile)){
-                    $user->setPicture($currentFile);
-                }
-            }
-            $user->setSlug('');
+    
+        $user = $this->getUser(); 
+        $user->setFirstName($request->request->get('firstName'));
+        $user->setLastName($request->request->get('lastName'));
+        $user->setPresentation($request->request->get('presentation'));
+ 
+            // die(dump($password));
+            // return $password;
             $manager->persist($user);
-            $manager->flush();
-         
-        }
-
-        // return $this->render("account/profile.html.twig",[
-        //     'myProfileForm' => $form->createView()
-        // ]);
-
-
+            $manager->flush(); 
+            return $user;
     }
 
 
-     /**
-     * Permet de modifier l'avatar de l'utilisateur
-     * @Route("/account/imgmodify", name="account_modifimg")
-     * @IsGranted("ROLE_USER")
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @return Response
-     */
-    public function imgModify(Request $request, EntityManagerInterface $manager){
-        $imgModify = new UserImgModify();
-        $user = $this->getUser();
-        $form = $this->createForm(ImgModifyType::class, $imgModify);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            if(!empty($user->getPicture())){
-                unlink($this->getParameter('uploads_directory').'/'.$user->getPicture());
-            }
-            $file = $form['newPicture']->getData();
-            if(!empty($file)){
-                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                $safeFilename = $originalFilename.'-'.rand();
-                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
-                try{
-                    $file->move(
-                        $this->getParameter('uploads_directory'),
-                        $newFilename
-                    );
-                }
-                catch(FileException $e)
-                {
-                    return $e->getMessage();
-                }
-
-                $user->setPicture($newFilename);
-            }
-
-            $manager->persist($user);
-            $manager->flush();
-     
-        }
-
-        // return $this->render("account/imgModify.html.twig",[
-        //     'form' => $form->createView()
-        // ]);
-
-    }
-
-    /**
-     * @IsGranted("ROLE_USER")
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @param UserPasswordEncoderInterface $encoder
-     * @return Response
-     */
-    public function updatePassword(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
-    {
-        $passwordUpdate = new PasswordUpdate();
-        $user = $this->getUser();
-        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            // vérifier que le mot de passe correspond à l'ancien
-            if(!password_verify($passwordUpdate->getOldPassword(),$user->getPassword())){
-                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez tapé n'est pas votre mot de passe actuel"));
-            }else{
-                $newPassword = $passwordUpdate->getNewPassword();
-                $hash = $encoder->encodePassword($user,$newPassword);
-
-                $user->setPassword($hash);
-                $manager->persist($user);
-                $manager->flush();
-             
-
-                return $this->redirectToRoute('account_index');
-
-            }
-        }
-
-    }
 
 
 
